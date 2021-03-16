ff2e76cf4fab9d083059047a44d9596a1bdcaeb1
arenasxiong <arenasxiong@tencent.com>
2021-03-16 17:08:29 +0800

    fix: bugs

--- alpha-tool/alphaVideo.py
+++ alpha-tool/alphaVideo.py
@@ -6,11 +6,7 @@ import subprocess
 import sys
 import shutil
 import argparse
-import cv2
-import numpy as np
 
-isDebug = False
-needZip = False
 outputVideoPath = ""
 imageDir = ""
 srcPath = ""
@@ -22,47 +18,31 @@ bitrate = 2000
 
 
 def main():
+	print("转换开始")
 	parser = argparse.ArgumentParser(description='manual to this script')
 	parser.add_argument('--file', type=str, default = None)
 	parser.add_argument('--dir', type=str, default = None)
-	parser.add_argument('--zip', type=str2bool, nargs='?', const=True, default = False, help="Activate zip mode.")
 	parser.add_argument('--fps', type=int, default = 30)
 	parser.add_argument('--bitrate', type=int, default = 2000)
 	args = parser.parse_args()
-
-	print("convertAlphaVideo.py running")
-	global needZip
-	needZip = args.zip
+	
 	fps = args.fps
 	bitrate = args.bitrate
-	# print "args.zip: ", args.zip
 	if not args.file is None:
 		parseVideoFile(args.file)
 	elif not args.dir is None:
 		parseImageDir(args.dir)
 	else:
-		print("params is None!")
+		print("没有输入")
 		return
-	print("finish")
-
-
-def str2bool(v):
-    if isinstance(v, bool):
-       return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
-
+	print("转换完毕")
 
 def help():
 	print("help ~")
 
 
 def parseVideoFile(path):
-	print(">>>>>>> paraseVideoFile, file is %s" % path)
+	print("视频文件 %s" % path)
 
 	parentDir = os.path.basename(path)
 	parentDir = parentDir.split('.')[0] + "/"
@@ -73,19 +53,19 @@ def parseVideoFile(path):
 	imagesToVideo(outputPath, oVideoFilePath)
 
 	shutil.rmtree(parentDir + "temp/")
-	print(">>>>>> convert alpha video finish, video file path is : %s" % oVideoFilePath)
+	print("视频转换完成，请查看: %s" % oVideoFilePath)
 
 
 def parseImageDir(path):
 	parentDir = os.path.abspath(path) + "/"
-	print(">>>>>>> paraseImageDir, dirName is %s" % parentDir)
+	print("图片目录 %s" % parentDir)
 
 	initDir(parentDir)
 	parseImageList(parentDir)
 	imagesToVideo(outputPath, oVideoFilePath)
 
 	shutil.rmtree(parentDir + "temp/")
-	print(">>>>>> convert alpha video finish, video file path is : %s" % oVideoFilePath)
+	print("合成完毕，请查看 : %s" % oVideoFilePath)
 
 
 def initDir(parentDir):
@@ -122,11 +102,7 @@ def parseImageList(inputPath):
 			outputImageFile = outputPath + os.path.splitext(fileName)[0] + ".jpg"
 
 			removeAlpha(inputImageFile, srcImageFile)
-			if needZip:
-				separateAlphaChannel(inputImageFile, tempMaskImageFile)
-				zipAlphaChannelPro(tempMaskImageFile, maskImageFile)
-			else:
-				separateAlphaChannel(inputImageFile, maskImageFile)
+			separateAlphaChannel(inputImageFile, maskImageFile)
 			appendImageLand(srcImageFile, maskImageFile, outputImageFile)
 
 			deleteTempFile(srcImageFile)
@@ -139,66 +115,25 @@ def parseImageList(inputPath):
 
 def videoToImage(videoPath, imageDir):
 	command = "ffmpeg -i {} -r {} {}%03d.png".format(videoPath, fps, imageDir)
-	if isDebug:
-		print (command)
 	ret = subprocess.Popen(command, shell = True)
 	ret.communicate()
 
 
 def removeAlpha(imageSrc, imageDst):
-	command = "convert {} -background black -alpha remove {}".format(imageSrc, imageDst)
-	if isDebug:
-		print (command)
+	command = "convert {} -background black -alpha off {}".format(imageSrc, imageDst)
 	ret = subprocess.Popen(command, shell = True)
 	ret.communicate()
 
 
 def separateAlphaChannel(imageFileOne, imageFileTwo):
 	command = "convert {} -channel A -separate {}".format(imageFileOne, imageFileTwo)
-	if isDebug:
-		print (command)
 	ret = subprocess.Popen(command, shell = True)
 	ret.communicate()
 
 
-def zipAlphaChannel(imageSrc, imageDst):
-	srcImage = cv2.imread(imageSrc)
-	shape = srcImage.shape
-	dstImage = np.zeros((int(shape[0]), int(shape[1])/3, int(shape[2])), np.uint8)
-	dstShape = dstImage.shape
-
-	height 		= dstShape[0]
-	width 		= dstShape[1]
-	channels 	= dstShape[2]
-
-	for row in range(height):
-		for col in range(width):
-			for channel in range(channels):
-				dstImage[row][col][channel] = srcImage[row][col * 3 + channel][0]
-	cv2.imwrite(imageDst, dstImage)
-
-
-def zipAlphaChannelPro(imageSrc, imageDst):
-	srcImage = cv2.imread(imageSrc)
-	shape = srcImage.shape
-	dstImage = np.zeros((int(shape[0]), int(shape[1])/3, int(shape[2])), np.uint8)
-	dstShape = dstImage.shape
-
-	height 		= dstShape[0]
-	width 		= dstShape[1]
-	channels 	= dstShape[2]
-
-	for row in range(height):
-		for col in range(width):
-			for channel in range(channels):
-				dstImage[row][col][channel] = srcImage[row][col + channel * width][0]
-	cv2.imwrite(imageDst, dstImage)
-
 
 def appendImageLand(imageFileOne, imageFileTwo, imageFileAppend):
 	command = "convert +append {} {} {}".format(imageFileOne, imageFileTwo, imageFileAppend)
-	if isDebug:
-		print (command)
 	ret = subprocess.Popen(command, shell = True)
 	ret.communicate()
 
@@ -210,15 +145,13 @@ def deleteTempFile(filePath):
 
 def imagesToVideo(imagesPath, videoFile):
 	command = "ffmpeg -r {} -i {}%03d.jpg -vcodec libx264 -pix_fmt yuv420p -b {}k {}".format(fps, imagesPath, bitrate, videoFile)
-	if isDebug:
-		print (command)
 	ret = subprocess.Popen(command, shell = True)
 	ret.communicate()
 
 
 def updateProgress(progress, total):
 	percent = round(1.0 * progress / total * 100,2)
-	sys.stdout.write('\rprogress : %s [%d/%d]'%(str(percent)+'%', progress, total))
+	sys.stdout.write('\r进度 : %s [%d/%d]'%(str(percent)+'%', progress, total))
 	sys.stdout.flush()
 
 
